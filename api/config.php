af1ce5ee0f66999f98b708a250586a1498bbd49a
Kin Lane <info@apievangelist.com>
2015-09-05 11:14:43 -0700

    fix db source

--- api/config.php
+++ api/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $dbserver = "laneworks-2.cjgvjastiugl.us-east-1.rds.amazonaws.com";
-$dbname = "stack_network_kinlane_projectapi";
-//$dbname = "apievangelist";
+//$dbname = "stack_network_kinlane_projectapi";
+$dbname = "apievangelist";
 $dbuser = "kinlane";
 $dbpassword = "ap1stack!";
 
