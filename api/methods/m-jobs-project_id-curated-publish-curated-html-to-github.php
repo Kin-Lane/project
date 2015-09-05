50bd20ad4f00ffc900f619c6ac2911ffbb8556c0
Kin Lane <info@apievangelist.com>
2015-09-05 15:28:26 -0700

    debug

--- api/methods/m-jobs-project_id-curated-publish-curated-html-to-github.php
+++ api/methods/m-jobs-project_id-curated-publish-curated-html-to-github.php
@@ -13,7 +13,7 @@ $app->get($route, function ($project_id)  use ($app,$appid,$appkey,$guser,$gpass
 	//echo $project_id;
 
 	$ProjectQuery = "SELECT * FROM project WHERE Project_ID = " . $project_id;
-	//echo $ProjectQuery;
+	echo "P:" . $ProjectQuery;
 	$ProjectResults = mysql_query($ProjectQuery) or die('Query failed: ' . mysql_error());
 	if($ProjectResults && mysql_num_rows($ProjectResults))
 		{
@@ -32,6 +32,7 @@ $app->get($route, function ($project_id)  use ($app,$appid,$appkey,$guser,$gpass
 		$TagQuery .= " INNER JOIN project_tag_pivot ptp ON t.tag_id = ptp.tag_id";
 		$TagQuery .= " WHERE ptp.Project_ID = " . $project_id;
 		$TagQuery .= " ORDER BY t.tag DESC";
+		echo "T)" . $TagQuery;
 		$TagResult = mysql_query($TagQuery) or die('Query failed: ' . mysql_error());
 
 		while ($Tag = mysql_fetch_assoc($TagResult))
