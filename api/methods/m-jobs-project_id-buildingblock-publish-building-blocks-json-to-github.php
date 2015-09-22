0e6926b353fef3290fe6d3ec00f8a0a1bbc22f47
Kin Lane <info@apievangelist.com>
2015-09-21 22:03:26 -0700

    debug doh

--- api/methods/m-jobs-project_id-buildingblock-publish-building-blocks-json-to-github.php
+++ api/methods/m-jobs-project_id-buildingblock-publish-building-blocks-json-to-github.php
@@ -40,7 +40,7 @@ $app->get($route, function ($project_id)  use ($app,$appid,$appkey,$guser,$gpass
 			//echo $thistag . "<br />";
 
 			$url = "http://buildingblock.api.kinlane.com/buildingblocks/tags/" . urlencode($thistag) . "/?appid=" . $appid . "&appkey=" . $appkey;
-			//echo $url . "<br />";
+			echo $url . "<br />";
 
 			$http = curl_init();
 			curl_setopt($http, CURLOPT_URL, $url);
