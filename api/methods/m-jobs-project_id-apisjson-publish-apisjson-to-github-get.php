51e2035840c2bfd9e490e98998f30bb3956daed3
Kin Lane <info@apievangelist.com>
2015-12-16 17:34:06 -0800

    debug

--- api/methods/m-jobs-project_id-apisjson-publish-apisjson-to-github-get.php
+++ api/methods/m-jobs-project_id-apisjson-publish-apisjson-to-github-get.php
@@ -110,15 +110,6 @@ $app->get($route, function ($project_id)  use ($app,$appid,$appkey,$guser,$gpass
 
       			// Base URL
       			$Base_URL = "";
-      			$query = "SELECT Company_URL_ID,Type,URL FROM company_url WHERE Company_ID = " . $organization_id . " AND Type = 'BaseURL'";
-      			$linkResult = mysql_query($query) or die('Query failed: ' . mysql_error());
-      			if($linkResult && mysql_num_rows($linkResult))
-      				{
-      				while ($link = mysql_fetch_assoc($linkResult))
-      					{
-      					$Base_URL = $link['URL'];
-      					}
-      				}
 
       			// Begin Individual APIs.json
       			$APIJSON = array();
@@ -135,15 +126,6 @@ $app->get($route, function ($project_id)  use ($app,$appid,$appkey,$guser,$gpass
       			array_push($Tags, $Tag);
 
       			$Tags = array();
-      			$TagQuery = "SELECT DISTINCT t.Tag FROM tags t JOIN company_tag_pivot ctp ON t.Tag_ID = ctp.Tag_ID WHERE ctp.Company_ID = " . $organization_id . " AND t.Tag NOT LIKE '%-Stack' ORDER BY t.Tag";
-      			//echo $TagQuery;
-      			$TagResult = mysql_query($TagQuery) or die('Query failed: ' . mysql_error());
-      			$rowcount = 1;
-      			while ($ThisTag = mysql_fetch_assoc($TagResult))
-      				{
-      				$Tag = strtolower($ThisTag['Tag']);
-      				array_push($Tags, $Tag);
-      				}
 
       			$APIJSON['tags'] = $Tags;
 
@@ -175,138 +157,11 @@ $app->get($route, function ($project_id)  use ($app,$appid,$appkey,$guser,$gpass
 
       			$API['properties'] = array();
 
-      			$CompanyURLQuery = "SELECT * FROM company_url WHERE Company_ID = " . $organization_id . " ORDER BY Name, Type";
-      			//echo $CompanyURLQuery . "<br />";
-      			$CompanyURLResult = mysql_query($CompanyURLQuery) or die('Query failed: ' . mysql_error());
-
-      			while ($CompanyURL = mysql_fetch_assoc($CompanyURLResult))
-      				{
-      				$Company_URL_ID = $CompanyURL['Company_URL_ID'];
-
-      				$API_URL = $CompanyURL['URL'];
-      				$API_URL_Name = $CompanyURL['Name'];
-      				$API_URL_Type = $CompanyURL['Type'];
-
-      				$API_Building_Block_ID = $CompanyURL['Building_Block_ID'];
-
-      				$API_Building_Block_Name = "";
-      				$API_Building_Block_Description = "";
-      				$API_Building_Block_Icon = "";
-
-      				if($API_Building_Block_ID>0)
-      					{
-
-      					$Building_Block_Query = "SELECT Building_Block_ID, bb.Name AS Building_Block_Name, bb.About, bbc.Name AS Building_Block_Category_Name, bbc.Type as Type FROM building_block bb JOIN building_block_category bbc ON bb.Building_Block_Category_ID = bbc.BuildingBlockCategory_ID WHERE Building_Block_ID = " . $API_Building_Block_ID;
-      					//echo $Building_Block_Query . "<br />";
-      					$Building_Block_Result = mysql_query($Building_Block_Query) or die('Query failed: ' . mysql_error());
-      					if($Building_Block_Result && mysql_num_rows($Building_Block_Result))
-      						{
-      						$HaveBuildingBlock = 1;
-      						$Building_Block = mysql_fetch_assoc($Building_Block_Result);
-
-      						$Building_Block_Image_Query = "SELECT Image_Name,Image_Path FROM building_block_image WHERE Image_Path <> '' AND Building_Block_ID = " . $API_Building_Block_ID . " ORDER BY Building_Block_Image_ID DESC";
-      						$Building_Block_Image_Result = mysql_query($Building_Block_Image_Query) or die('Query failed: ' . mysql_error());
-      						while ($Building_Block_Image = mysql_fetch_assoc($Building_Block_Image_Result))
-      							{
-      							$API_Building_Block_Icon = $Building_Block_Image['Image_Path'];
-      							}
-
-      						$API_Building_Block_Name = $Building_Block['Building_Block_Name'];
-      						//echo "Building Block Name: " . $API_Building_Block_Name . "<br />";
-      						$API_Building_Block_Description = $Building_Block['About'];
-
-      						}
-
-      					$API_URL_Type_Slug = PrepareFileName($API_Building_Block_Name);
-
-      					$Link = array();
-      					$Link['type'] = "X-" . $API_URL_Type_Slug;
-      					$Link['url'] = trim($API_URL);
-      					array_push($API['properties'], $Link);
-
-      					}
-      				}
-
       			array_push($APIJSON['apis'], $API);
 
       			$APIJSON['include'] = array();
 
-      			// Begin APIs
-      			$APIQuery = "SELECT * FROM api WHERE Company_ID = " . $organization_id . " ORDER BY Name";
-      			//echo $TagQuery;
-      			$APIResult = mysql_query($APIQuery) or die('Query failed: ' . mysql_error());
-      			$rowcount = 1;
-
-      			if($APIResult && mysql_num_rows($APIResult))
-      				{
-      				while ($API = mysql_fetch_assoc($APIResult))
-      					{
-
-      					$API_ID = $API['API_ID'];
-      					$API_Name = $API['Name'];
-      					$API_About = $API['About'];
-
-      					$API_About = str_replace(chr(34),"",$API_About);
-      					$API_About = str_replace(chr(39),"",$API_About);
-      					$API_About = strip_tags($API_About);
-      					$API_About = mysql_real_escape_string($API_About);
-
-      					// Website
-      					$API_Website_URL = "";
-      					$query = "SELECT URL FROM api_url WHERE API_ID = " . $API_ID . " AND Type = 'Website'";
-      					$linkResult = mysql_query($query) or die('Query failed: ' . mysql_error());
-      					if($linkResult && mysql_num_rows($linkResult))
-      						{
-      						while ($link = mysql_fetch_assoc($linkResult))
-      							{
-      							$API_Website_URL = $link['URL'];
-      							}
-      						}
-
-      					$Include = array();
-      					$Include['name'] = $API_Name;
-      					$Include['url'] = $API_Website_URL;
-      					array_push($APIJSON['include'], $Include);
-
-      					}
-
-      				$APIJSON['maintainers'] = array();
-
-      				$Maintainer = array();
-      				$Maintainer['FN'] = "Kin";
-      				$Maintainer['X-twitter'] = "apievangelist";
-      				$Maintainer['email'] = "kin@email.com";
-
-      				array_push($APIJSON['maintainers'], $Maintainer);
-
-      				$ReturnEachAPIJSON = stripslashes(format_json(json_encode($APIJSON)));
-
-      				$API['contact'] = array();
-      				$Contact = array();
-      				$Contact['FN'] = $Company_Name;
-      				if($Email_Address!='')
-      					{
-      					$Contact['email'] = trim(str_replace("mailto:","",$Email_Address));
-      					}
-
-      				if($twitter_url!='')
-      					{
-      					$Contact['X-twitter'] = $twitter_url;
-      					}
-      				array_push($API['contact'], $Contact);
-
-      				array_push($APIJSON['apis'], $API);
-
-      				$APIJSON['maintainers'] = array();
-
-      				$Maintainer = array();
-      				$Maintainer['FN'] = "Kin";
-      				$Maintainer['X-twitter'] = "apievangelist";
-      				$Maintainer['email'] = "kin@email.com";
-
-      				array_push($APIJSON['maintainers'], $Maintainer);
-
-      				}
+      			
       			}
       		}
         }
