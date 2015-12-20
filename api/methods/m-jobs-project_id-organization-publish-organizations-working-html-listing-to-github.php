2714bdab6a0912174698a880fba72b57630cba15
Kin Lane <info@apievangelist.com>
2015-12-20 00:40:19 -0800

    debug

--- api/methods/m-jobs-project_id-organization-publish-organizations-working-html-listing-to-github.php
+++ api/methods/m-jobs-project_id-organization-publish-organizations-working-html-listing-to-github.php
@@ -236,7 +236,7 @@ $app->get($route, function ($project_id)  use ($app,$appid,$appkey,$guser,$gpass
 
 		    			//$row .= '<td align="center" width="50">' . chr(10);
 							///$row .= '</td>' . chr(10);
-							$row .= '<td align="left" style="font-size: 11px; padding-left: 20px;" width="50%">' . chr(10);
+							$row .= '<td align="left" style="font-size: 11px; padding-left: 20px;" width="35%">' . chr(10);
 							$row .= '' . $API_Name . '';
 							$row .= '</td>' . chr(10);
 
