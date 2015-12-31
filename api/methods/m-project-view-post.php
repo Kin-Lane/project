b0c212b2b066ea31697e6f6e22bfa4a4ee834b08
Kin Lane <info@apievangelist.com>
2015-12-30 21:17:37 -0800

    debug

--- api/methods/m-project-view-post.php
+++ api/methods/m-project-view-post.php
@@ -6,10 +6,13 @@ $app->post($route, function () use ($app){
 
  	$request = $app->request();
  	$params = $request->params();
-	var_dump($params);
-	//if(isset($params['title'])){ $title = mysql_real_escape_string($params['title']); } else { $title = 'No Title'; }
-	echo "<hr />";
-	var_dump($_SERVER);
+
+	$HTTP_REFERER = $_SERVER['HTTP_REFERER'];
+	echo $HTTP_REFERER . chr(10);
+	$base_url_array = parse_url($HTTP_REFERER);
+	$base_host = $base_url_array['host'];
+
+	echo $base_host . chr(10);
 
 	//$app->response()->header("Content-Type", "application/json");
 	//echo format_json(json_encode($ReturnObject));
