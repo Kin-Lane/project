dfa5908662f7765754f4ca21d479d9458f5c2439
Kin Lane <info@apievangelist.com>
2015-12-30 20:50:50 -0800

    debug

--- api/methods/m-project-view-post.php
+++ api/methods/m-project-view-post.php
@@ -6,9 +6,9 @@ $app->post($route, function () use ($app){
 
  	$request = $app->request();
  	$params = $request->params();
-
+	var_dump($params);
 	//if(isset($params['title'])){ $title = mysql_real_escape_string($params['title']); } else { $title = 'No Title'; }
-
+	echo "<hr />";
 	var_dump($_SERVER);
 
 	//$app->response()->header("Content-Type", "application/json");
